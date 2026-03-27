ffb37d4809908049bfff18424b1cdf47ba8220a3
AviatrixCommunications <151691863+AviatrixCommunications@users.noreply.github.com>
2026-03-27 11:34:33 -0500

    Add files via upload

--- plgc-build-work/functions.php
+++ plgc-build-work/functions.php
@@ -14,7 +14,7 @@ defined('ABSPATH') || exit;
 /**
  * Constants
  */
-define('PLGC_VERSION', '1.6.35');
+define('PLGC_VERSION', '1.6.37');
 define('PLGC_DIR', get_stylesheet_directory());
 define('PLGC_URI', get_stylesheet_directory_uri());
 
